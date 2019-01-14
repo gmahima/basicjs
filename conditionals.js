93ee24f1ce2983f374f82592bb3eb1f137a1b464
mahima gangavarapu <mahimagangavarapu@gmail.com>
2019-01-14 22:09:36 +0530

    add switch

---
+++ conditionals.js
@@ -0,0 +1,47 @@
+var day = 8;
+var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+//
+// if(day === 0) {
+// 	console.log('Sunday');
+// } else if (day === 1) {
+// 	console.log('Monday');
+// } else if (day === 2) {
+// 	console.log('Tuesday');
+// } else if (day === 3) {
+// 	console.log('Wednesday');
+// } else if (day === 4) {
+// 	console.log('Thursday');
+// } else if (day === 5) {
+// 	console.log('Friday');
+// } else if (day === 6) {
+// 	console.log('Saturday');
+// } else {
+// 	console.log('Invalid Day');
+// }
+switch(day) {
+  case 0:
+  console.log(daysOfWeek[0]);
+  break;
+  case 1:
+  console.log(daysOfWeek[1]);
+  break;
+  case 2:
+  console.log(daysOfWeek[2]);
+  break;
+  case 3:
+  console.log(daysOfWeek[3]);
+  break;
+  case 4:
+  console.log(daysOfWeek[4]);
+  break;
+  case 5:
+  console.log(daysOfWeek[5]);
+  break;
+  case 6:
+  console.log(daysOfWeek[6]);
+  break;
+  default:
+  console.log('Invalid');
+  break;
+
+}
